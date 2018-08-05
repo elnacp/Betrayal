d8b0432163018068d0b1a91cf504e92d431cee33
elnacabot <ls30917@salleurl.edu>
2018-08-05 17:55:46 +0200

    Timer incluido cuenta atras de 15 ya puesto

---
+++ Betrayal/Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text text;
+    float theTime;
+    public float speed;
+    public float time = 840;
+
+    void Start()
+    {
+        
+        text = GetComponent<Text>();
+    }
+    void Update()
+    {
+        time -= Time.deltaTime * speed;
+        string minutes = ((time % 3600) / 60).ToString("00");
+        string seconds = (time % 60).ToString("00");
+        text.text = minutes + ":" + seconds;
+        /*theTime = Time.deltaTime * speed;
+        string minutes = ((theTime % 3600)/60).ToString("00");
+        string seconds = (theTime % 60).ToString("00");
+        text.text = minutes + ":" + seconds;*/
+
+    }
+
+
+
+}
